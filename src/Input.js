6b789da417d018c56b9fff754d4ccb0d9d04cb1a
Hayoun Lee <sidhlee@gmail.com>
2020-02-13 20:25:14 +0900

    Add test for input-state biding in Input component
    testing useState hook

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-control"
+          className="form-control"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={e => setCurrentGuess(e.target.value)}
+        />
+        <button data-test="submit-button" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
--- src/Input.test.js
+++ src/Input.test.js
@@ -26,3 +26,18 @@ it('does not throw warning with expected props', () => {
   const conformingProps = defaultProps;
   checkProps(Input, conformingProps);
 });
+
+describe('state controlled input field', () => {
+  test('state updates with value of input control upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    // replace useState with mock function
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+    const wrapper = setup();
+    const inputControl = findByTestAttr(wrapper, 'input-control');
+
+    const mockEvent = { target: { value: 'conga' } };
+    inputControl.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('conga');
+  });
+});
