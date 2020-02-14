5ab15a3337874a23e30c9d13237b36184ae64df3
Hayoun Lee <sidhlee@gmail.com>
2020-02-14 17:03:42 +0900

    Add test for fetching secretWord in App component
    testing api call within useEffect with mockModule

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import './App.css';
 
+import hookActions from './actions/hookActions';
+
+// App.test will swap hookActions.getSecretWord with mock here
+
+// state is updated when dispatch is called with action
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSecretWord':
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
+
 function App() {
+  const initialState = { secretWord: null };
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  // dispatch action with secretWord
+  const setSecretWord = secretWord =>
+    dispatch({ type: 'setSecretWord', payload: secretWord });
+
+  React.useEffect(
+    // call API and run setSecretWord passing response.data
+    () => {
+      hookActions.getSecretWord(setSecretWord);
+    },
+    [] // this will only run once on mount and not on re-render
+  );
   return <div data-test="component-app"></div>;
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+// Enzyme currently(Feb 2020) doesn't run useEffect on shallow
+// React recommends using React Testing Library
+// https://reactjs.org/docs/testing.html#tools
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
+// we'll mock this module
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
 /**
  * Setup function for App component
- * @returns {Object} - ShallowWrapper
+ * @returns {Object} - ReactWrapper
  */
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount, because useEffect not called on 'shallow'
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
 test('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord', () => {
+  it('gets called on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
